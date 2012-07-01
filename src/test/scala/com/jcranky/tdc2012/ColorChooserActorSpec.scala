52c6e751319b030be66217b2f5fec1df5fe5c1fc
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-01 19:24:36 -0300

    now doing a functional test of the actor only - multiples probes are not working well together for unit testing

--- src/test/scala/com/jcranky/tdc2012/ColorChooserActorSpec.scala
+++ src/test/scala/com/jcranky/tdc2012/ColorChooserActorSpec.scala
@@ -1,26 +1,24 @@
 package com.jcranky.tdc2012
 
+import akka.pattern.ask
 import akka.testkit.TestActorRef
-import akka.testkit.TestProbe
+import akka.util.Timeout
+import java.awt.Color
 import org.specs2.mutable.Specification
 
 class ColorChooserActorSpec extends Specification {
   "the color chooser actor" should {
-    "ask for the individual color components from its child actors" in new ActorScope {
-      val rProbe = TestProbe()(system)
-      val gProbe = TestProbe()
-      val bProbe = TestProbe()
+    "ask for the individual color components from its child actors and send a color back to the caller" in new ActorScope {
+      val colorChooserActor = TestActorRef(new ColorChooserActor(
+          TestActorRef[ColorPartChooserActor],
+          TestActorRef[ColorPartChooserActor],
+          TestActorRef[ColorPartChooserActor]
+        ))
+      val futureColor = colorChooserActor.ask(FindColor(Position(0, 0)))(Timeout(3000)).mapTo[ColorFound]
       
-      val colorChooserActor = TestActorRef(new ColorChooserActor(rProbe.ref, gProbe.ref, bProbe.ref))
-      colorChooserActor ! FindColor(Position(0, 0))
-      
-      rProbe.expectMsg(FindColorPart); rProbe.sender ! ColorPartFound(0.1f)
-      gProbe.expectMsg(FindColorPart); gProbe.sender ! ColorPartFound(0.2f)
-      bProbe.expectMsg(FindColorPart); bProbe.sender ! ColorPartFound(0.3f)
-    }
-    
-    "return ColorFound with a new color for the specified position" in new ActorScope {
-      pending
+      futureColor.isCompleted must beTrue
+      futureColor.value.get.right.get.pos must_== Position(0, 0)
+      futureColor.value.get.right.get.color must beAnInstanceOf[Color]
     }
   }
 }
