76c2f10dd81ef44493f056118f5471e401277f83
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-06 22:36:22 -0300

    now sending a list of find colors instead of just one

--- src/main/scala/com/jcranky/tdc2012/messages.scala
+++ src/main/scala/com/jcranky/tdc2012/messages.scala
@@ -6,7 +6,7 @@ case object StartColorPicking
 
 case class FindColorForRange(initialPos: Position, finalPos: Position)
 
-case class FindColor(pos: Position)
+case class FindColor(pos: List[Position])
 case class ColorFound(pos: Position, rgb: Tuple3[Float, Float, Float])
 case class ColorsFound(colors: List[ColorFound])
 
